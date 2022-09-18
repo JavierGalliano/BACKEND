a5aa4a60a96b87205c96e58658b545a2cff6d870
Javier Galliano <javiergalliano1@gmail.com>
2022-09-18 15:21:10 -0300

    desafio 4

---
+++ data/data.js
@@ -0,0 +1,36 @@
+const products = [
+  {
+    id: 1,
+    name: 'Perfume',
+    description: 'Perfume con fragancia a jazmin',
+    price: 323.45,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Squadra_45.jpg'
+  },
+  {
+    id: 2,
+    name: 'Pelota',
+    description: 'Pelota mundial de quatar',
+    price: 234.56,
+    image: 'https://www.alairelibre.cl/noticias/site/artic/20191209/imag/foto_0000000120191209162021.jpg'
+  },
+  {
+    id: 3,
+    name: 'Camiseta Argentina',
+    description: 'Camiseta Seleccion Argentina',
+    price: 45.67,
+    image: 'https://th.bing.com/th/id/R.fa672b12997a45e7d9a9847a512edce6?rik=SeV2%2bE46YmjGpw&pid=ImgRaw&r=0'
+  },
+  {
+    id: 4,
+    name: 'Botines ',
+    description: 'Botines que juegan solos',
+    price: 456.78,
+    image: 'https://th.bing.com/th/id/R.0b946075b5984decfebd917594f0fdaa?rik=o8YoYHA2s8ZPrg&pid=ImgRaw&r=0'
+  },
+];
+
+
+
+module.exports = {
+  products
+};
